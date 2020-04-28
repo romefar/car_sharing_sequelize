aded139b074a72815a07ccc67e4537a5b731b6e7
romefar <vlad.romanovr@gmail.com>
2020-04-28 20:29:34 +0300

    updated driver model

--- src/models/driver.js
+++ src/models/driver.js
@@ -4,21 +4,36 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       unique: true,
       validate: {
-        isAlphanumeric: true
+        is: {
+          args: [/^[0-9A-Z-]+$/g],
+          msg: 'The driver license can only contain letters, numbers and hyphen characters.'
+        }
       }
     },
     firstName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: 'Empty first name is not allowed.'
+        },
+        is: {
+          args: [/^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/g],
+          msg: 'First name can only contain the following characters: a-z, A-Z, \', hyphen and space.'
+        }
       }
     },
     lastName: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: 'Empty last name is not allowed.'
+        },
+        is: {
+          args: [/^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/g],
+          msg: 'Last name can only contain the following characters: a-z, A-Z, \', hyphen and space.'
+        }
       }
     }
   })
