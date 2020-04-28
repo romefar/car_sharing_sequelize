a47136cb1fd2ad8b8622a8b6599d34f9b1913b37
romefar <vlad.romanovr@gmail.com>
2020-04-28 20:28:29 +0300

    updated booking model

--- src/models/booking.js
+++ src/models/booking.js
@@ -2,16 +2,46 @@ module.exports = (sequelize, DataTypes) => {
   const Booking = sequelize.define('booking', {
     finishFuelLevel: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       validate: {
         min: 0
       }
     },
     finishMileage: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       validate: {
         min: 0
+      }
+    }
+  }, {
+    hooks: {
+      async afterCreate (instance) {
+        const { dataValues: { carId }, sequelize: { models: { car: carModel } } } = instance
+        const car = await carModel.findByPk(carId)
+        car.status = 'Reserved'
+        await car.save()
+
+        // TODO: change run start fuel and mileage data
+      },
+      async beforeUpdate (instance) {
+        const {
+          dataValues: { finishFuelLevel, finishMileage, carId, runId },
+          sequelize: { models: { car: carModel, run: runModel } }
+        } = instance
+
+        if (finishFuelLevel !== 0 && finishMileage !== 0) {
+          const car = await carModel.findByPk(carId)
+          const { fuelCapacity, mileage } = car
+          if (finishFuelLevel > fuelCapacity || finishMileage < mileage) {
+            throw new Error(`Invalid resources data. Tank capacity is ${fuelCapacity} litres but spent ${finishFuelLevel}. Car mileage is ${mileage} but finishMileage is ${finishMileage}.`)
+          }
+
+          await car.increment('useCounter', { by: 1 })
+          car.status = 'Free'
+          car.mileage = finishMileage
+          car.fuelLevel = finishFuelLevel
+          // TODO: change long/lat data
+          await car.save()
+        }
       }
     }
   })
