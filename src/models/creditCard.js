6b6181967adcb3df0fc7b2e1d7a696199dfaf35e
romefar <vlad.romanovr@gmail.com>
2020-04-28 20:29:12 +0300

    updated credit card model

--- src/models/creditCard.js
+++ src/models/creditCard.js
@@ -5,19 +5,37 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       allowNull: false,
       validate: {
-        isCreditCard: true
+        isCreditCard: {
+          msg: 'Invalid card number.'
+        }
       }
     },
     cardHolder: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: 'Empty card holder name is not allowed.'
+        },
+        is: {
+          args: [/^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$/g],
+          msg: 'Card holder name can only contain the following characters: a-z, A-Z, \', hyphen and space.'
+        }
       }
     },
     cardValidDate: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isAfter: {
+          args: [new Date().toJSON().slice(0, 10)],
+          msg: 'You cannot use an expired credit card.'
+        }
+      }
+    },
+    isAuthorized: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: 0
     }
   })
   return CreditCard
