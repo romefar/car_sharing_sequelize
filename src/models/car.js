a8ec3a54d6d51a9ee9edb2501ae68aad7e073f14
romefar <vlad.romanovr@gmail.com>
2020-04-28 20:28:48 +0300

    updated car model

--- src/models/car.js
+++ src/models/car.js
@@ -5,14 +5,12 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       allowNull: false,
       validate: {
-        isAlphanumeric: true,
+        isAlphanumeric: {
+          msg: 'Each character in the VIN must be either a letter from the alphabet or a number from 0 through 9.'
+        },
         isVin (value) {
-          if (value.length !== 17) {
-            throw new Error('Incorrect VIN length.')
-          } else {
-            if (!value.matches(/[A-HJ-NPR-Z0-9]{13}[0-9]{4}/)) {
-              throw new Error('Incorrect VIN value.')
-            }
+          if (value.length !== 17 || !value.match(/[A-HJ-NPR-Z0-9]{13}[0-9]{4}/g)) {
+            throw new Error('Invalid VIN.')
           }
         }
       }
@@ -22,50 +20,92 @@ module.exports = (sequelize, DataTypes) => {
       unique: true,
       allowNull: false,
       validate: {
-        isAlphanumeric: true
+        len: {
+          args: [5, 9],
+          msg: 'Invalid EU vehicle registration number. The number length must be between 5 and 9'
+        },
+        is: {
+          args: [/^[A-Z0-9-]+$/g],
+          msg: 'The registration number can only contain letters, numbers and hyphen characters.'
+        }
       }
     },
     brand: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: 'Empty brand name is not allowed.'
+        },
+        is: {
+          args: [/^[a-zA-Z-]+$/g],
+          msg: 'The brand name can only contain letters and hyphen characters.'
+        }
       }
     },
     model: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true
+        notEmpty: {
+          msg: 'Empty model name is not allowed.'
+        },
+        isAlphanumeric: {
+          msg: 'The model name can only contain letters and numbers characters.'
+        }
       }
     },
     productionDate: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        isAfter: {
+          args: ['2010-01-01'], // YYYY-MM-DD ISO 8601
+          msg: 'The car cannot be older than 10 years.'
+        }
+      }
     },
     status: {
-      type: DataTypes.ENUM('Free', 'In use', 'Unavailable', 'In service'),
+      type: DataTypes.ENUM('Free', 'In use', 'Unavailable', 'In service', 'Reserved'),
       defaultValue: 'Free'
     },
     fuelCapacity: {
       type: DataTypes.INTEGER,
-      defaultValue: 30,
+      allowNull: false,
       validate: {
-        min: 0
+        min: {
+          args: [0],
+          msg: 'Fuel capacity cannot be a negative number.'
+        },
+        isInt: {
+          msg: 'Fuel capacity must be an integer.'
+        }
       }
     },
     fuelLevel: {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        min: 0
+        min: {
+          args: [0],
+          msg: 'Fuel level cannot be a negative number.'
+        },
+        isInt: {
+          msg: 'Fuel level must be an integer.'
+        }
       }
     },
     mileage: {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        min: 0
+        min: {
+          args: [0],
+          msg: 'Mileage cannot be a negative number.'
+        },
+        isInt: {
+          msg: 'Mileage must be an integer.'
+        }
       }
     },
     geoLatitude: {
@@ -75,7 +115,29 @@ module.exports = (sequelize, DataTypes) => {
     geoLongtitude: {
       type: DataTypes.FLOAT,
       allowNull: false
+    },
+    useCounter: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: {
+        min: {
+          args: [0],
+          msg: 'Use counter cannot be a negative number.'
+        },
+        isInt: {
+          msg: 'Use couter must be an integer.'
+        }
+      }
     }
+  }, {
+    validate: {
+      checkFuelLevel () {
+        if (this.fuelLevel > this.fuelCapacity) {
+          throw new Error('Fuel level cannot be more than the fuel capacity.')
+        }
+      }
+    }
+    // TODO: add hook for checking fuel capacity and mielage
   })
   return Car
 }
