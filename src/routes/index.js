0e50317b64b5ede24cd0575882d5f55b48d6b291
romefar <vlad.romanovr@gmail.com>
2020-05-02 17:19:14 +0300

    updated routes entry point

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const carsRoute = require('./cars')
 const driversRoute = require('./drivers')
+const bookingRoute = require('./booking')
 
 module.exports = {
   carsRoute,
-  driversRoute
+  driversRoute,
+  bookingRoute
 }
