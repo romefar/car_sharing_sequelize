2359e11ddd2fd7567005b913df4e1ba28e3109c4
romefar <vlad.romanovr@gmail.com>
2020-05-19 18:10:16 +0300

    Created car status constants

---
+++ src/utils/carStatus.js
@@ -0,0 +1,13 @@
+const CAR_UNAVAILABLE = 'Unavailable'
+const CAR_IN_USE = 'In use'
+const CAR_IN_SERVICE = 'In service'
+const CAR_RESERVED = 'Reserved'
+const CAR_FREE = 'Free'
+
+module.exports = {
+  CAR_UNAVAILABLE,
+  CAR_IN_USE,
+  CAR_IN_SERVICE,
+  CAR_RESERVED,
+  CAR_FREE
+}
