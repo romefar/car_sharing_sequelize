b79fd5d97664b5cc4513dc6e77f6b09303a1a0c2
romefar <vlad.romanovr@gmail.com>
2020-05-19 18:09:51 +0300

    Created bookings status constants

---
+++ src/utils/bookingsStatus.js
@@ -0,0 +1,11 @@
+const BOOKING_BOOKED = 'Booked'
+const BOOKING_ACTIVE = 'Active'
+const BOOKING_CLOSED = 'Closed'
+const BOOKING_CANCELLED = 'Cancelled'
+
+module.exports = {
+  BOOKING_BOOKED,
+  BOOKING_ACTIVE,
+  BOOKING_CLOSED,
+  BOOKING_CANCELLED
+}
