ee46f30b0e3252e5d57a1f01d51cfeff8d277427
romefar <vlad.romanovr@gmail.com>
2020-05-19 20:55:43 +0300

    Updated db config/ updated host

--- src/config/config.js
+++ src/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: 'localhost',
+    host: process.env.DB_HOSTNAME,
     dialect: 'mysql'
   },
   test: {
